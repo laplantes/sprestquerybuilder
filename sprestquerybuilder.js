7d023e6e97d8a2ff73ca9ca263d79fdd5be30ff4
Scott LaPlante <laplas@mmc.org>
2021-04-21 16:09:47 -0400

    sprestquerybuilder.js - Added click events for each button, added variables that will be used to build the query preview.

--- sprestquerybuilder.js
+++ sprestquerybuilder.js
@@ -1,1 +1,49 @@
-document.getElementById('close-notice').addEventListener('click', () => { console.log('clicked');document.getElementById('info-bar').classList.add('info-bar-hidden') } )
+'use strict';
+
+let selects = [],
+	filters = {},
+	orderby = ``,
+	top = ``;
+
+// add event listeners for buttons
+
+// Clear the info bar message body
+document.getElementById('close-notice').addEventListener('click', () => {
+	console.log('clicked');
+	document.getElementById('info-bar-body').innerText='';
+});
+
+// Click event for select button
+document.getElementById('add-select-item').addEventListener('click', () => {
+	console.log('Add select item clicked');
+});
+
+// Click event for filter button
+document.getElementById('add-filter-item').addEventListener('click', () => {
+	console.log('Add filter item clicked');
+});
+
+// Click event for filter date button
+document.getElementById('add-date-filter-item').addEventListener('click', () => {
+	console.log('Add filter date item clicked');
+});
+
+// Click event for orderby button
+document.getElementById('add-orderby-item').addEventListener('click', () => {
+	console.log('Add oderby item clicked');
+});
+
+// Click event for add top button
+document.getElementById('add-top-item').addEventListener('click', () => {
+	console.log('Add top item clicked');
+});
+
+// Click event for generate query button
+document.getElementById('generate-query').addEventListener('click', () => {
+	console.log('Generate Query clicked');
+});
+
+// Click event for test url button
+document.getElementById('test-url').addEventListener('click', () => {
+	console.log('Test URL clicked');
+});

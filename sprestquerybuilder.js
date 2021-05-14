1aef691def1c105d1b826944820c928457977589
Scott LaPlante <laplas@mmc.org>
2021-05-14 16:01:14 -0400

    sprestquerybuilder.js - removed clearInfoBar and display functions as they are no longer needed now that there are toast messages. Added the filter section to the buildItemHtml function and the buildQueryString function. Added content to the add filter item function. Misc minor code refactoring for cleanup items.

--- sprestquerybuilder.js
+++ sprestquerybuilder.js
@@ -3,24 +3,20 @@
 let selects = {},
 	selectCount = 0,
 	filters = {},
-	filtersCount = 0,
+	filterCount = 0,
 	orderBy = ``,
 	topNumber;
-
-const clearInfoBar = () => {
-	document.getElementById('info-bar-body').innerText='';
-};
 
 /**
  * function for clearing the lists contents before displaying new contents
  * @param  {object} listToClear [the variable that represents the container to clean out]
  * @return {none}             [none]
  */
-const clearList = (listToClear) => {
-	while (listToClear.firstChild) {
-		listToClear.removeChild(listToClear.firstChild);
-	}
-};
+// const clearList = (listToClear) => {
+// 	while (listToClear.firstChild) {
+// 		listToClear.removeChild(listToClear.firstChild);
+// 	}
+// };
 
 const showToast = (title=`Enter a title`, message=`Enter a message, even a short one, but a long one is ok as well.`) => {
 	document.getElementById('toast-title').innerText = title;
@@ -30,45 +26,62 @@ const showToast = (title=`Enter a title`, message=`Enter a message, even a short
     setTimeout(() => { element.classList.remove('show'); }, 5000);
 }
 
-const displayMessage = (message) => {
-	let content = document.getElementById('info-bar-body').innerText='';
-	if (content) {
-		clearInfoBar();
-		document.getElementById('info-bar-body').innerText=`${message}`
-	} else {
-		document.getElementById('info-bar-body').innerText=`${message}`
-	}
-};
-
 const buildItemHtml = (type, itemData) => {
 	switch(type) {
 		case 'select':
 			return `
 				<div  id="${type}-${itemData.id}" class="box-item">
 					<div><b>${type}:</b> ${itemData.value}</div>
-					<span>X</span>
+					<span id="close-select-${itemData.id}">X</span>
 				</div>
 			`;
+			
+		case 'filter':
+			return `
+			<div  id="${type}-${itemData.id}" class="box-item">
+			<div><b>${type}:</b> ${itemData.name} <b>{${itemData.operator}}</b> ${itemData.value}</div>
+			<span id="close-filter-${itemData.id}">X</span>
+			</div>				
+			`;
+			
 		default:
-		console.log("error");
-		break;
+			console.log("error");
+			break;
 	}
 };
 
-const buildQueryString = (querystringSelects='', querystringFilters=``, listLibrayyName) => {
-	let filtersArray = [],
-		fullQueryString = `?`;
+//  TODO: make and or available as a choice for multiple filters
+const buildQueryString = (querystringSelects=``, querystringFilters=``) => {
+	let fullQueryString = `?`;
 
-	if (querystringSelects) {
+
+	if (Object.keys(querystringSelects).length > 0) {
 		let selectsArray = [];
 		// Populate the contents of the selectsArray
 		for (const item of Object.entries(querystringSelects)) {
-		selectsArray.push(item[1].value)
+			selectsArray.push(item[1].value)
 		}
 		const selectsString= selectsArray.join(',');
-		fullQueryString +=`select=`;
+		fullQueryString += `$select=`;
 		fullQueryString += `${selectsString}`;
+	}
 
+	if (Object.keys(querystringFilters).length > 0) {
+		let filtersArray = [];
+		// Populate the contents of the filterArray
+		for (const item of Object.entries(querystringFilters)) {
+			filtersArray.push(`${item[1].name} ${item[1].operator} ${item[1].value}`)
+		}
+		let filtersString = ``; 
+		if (filtersArray.length === 1) {
+			filtersString = `${filtersArray[0]}`;
+		} else {
+			for (let i=0; i < filtersArray.length; i++) {
+				(i+1 === filtersArray.length) ? filtersString += `(${filtersArray[i]})`	: filtersString += `(${filtersArray[i]}) and `;
+			}
+		}
+		fullQueryString += `$filter=`;
+		fullQueryString += `(${filtersString})`;
 	}
 
 	return fullQueryString;
@@ -77,14 +90,12 @@ const buildQueryString = (querystringSelects='', querystringFilters=``, listLibr
 // add event listeners for buttons
 // ################################
 // Clear the info bar message body
-document.getElementById('close-notice').addEventListener('click', clearInfoBar);
+//  TODO: add this to the docs for the info bar
+// document.getElementById('close-notice').addEventListener('click', clearInfoBar);
 
 // Click event for select button
 document.getElementById('add-select-item').addEventListener('click', () => {
-	// addSelect();
 
-	// Clear the info bar if there is a current message
-	(document.getElementById('info-bar-body').innerText='') ? clearInfoBar() : false;
 	// get the select input value
 	const selectValue = document.getElementById('select-column-name').value;
 	// Ensure there is content to add
@@ -100,13 +111,12 @@ document.getElementById('add-select-item').addEventListener('click', () => {
 		// Get the newly created HTML element
 		const newSelectItem = document.getElementById(`select-${selectCount}`);
 		// Add an event listener to the newly created items remove button for removing the item from the page and from the selects object
-		document.getElementById(`select-${selectCount}`).childNodes[3].addEventListener('click', () => { newSelectItem.remove(); delete selects[`${newSelect.id}`]; });
+		document.getElementById(`close-select-${selectCount}`).addEventListener('click', () => { newSelectItem.remove(); delete selects[`${newSelect.id}`]; });
 		// clear the input in the form in preperation for the next item to be added
-		document.getElementById('select-column-name').value="";
+		document.getElementById('select-column-name').value = "";
 		// Increment the select counter		
 		selectCount++;
 	} else {
-		displayMessage(`Please enter a column name to add to the select query`);
 		showToast(`Attention`, `Please enter a column name to add to the select query`);
 	}
 });
@@ -114,6 +124,33 @@ document.getElementById('add-select-item').addEventListener('click', () => {
 // Click event for filter button
 document.getElementById('add-filter-item').addEventListener('click', () => {
 	console.log('Add filter item clicked');
+	// get the filter values
+	const filterName = document.getElementById('filter-column-name').value,
+		filterOperator = document.getElementById('filter-operator').value,
+		filterValue = document.getElementById('filter-value').value;
+		if (filterName && filterOperator && filterValue) {
+			// build the new object using the global filterCount and the functionally scoped filter values
+			const newFilter = {id: filterCount, name: filterName, operator: filterOperator, value: filterValue};
+			// Call the function to build out the required HTML to add to the filter to the page
+			const newFilterHtml = buildItemHtml(`filter`, newFilter);
+			// Insert the newly created HTML
+			document.getElementById('query-box-preview').insertAdjacentHTML('beforeend', newFilterHtml);
+			// add the new filter into the filterss object
+			filters[`${filterCount}`] = newFilter;
+			// Get the newly created HTML element
+			const newFilterItem = document.getElementById(`filter-${filterCount}`);
+			// Add an event listener to the newly created items remove button for removing the item from the page and from the selects object
+			document.getElementById(`close-filter-${filterCount}`).addEventListener('click', () => { newFilterItem.remove(); delete filters[`${newFilter.id}`]; });
+			// clear the inputs in the form in preperation for the next item to be added
+			document.getElementById('filter-column-name').value = "";
+			document.getElementById('filter-operator').value = "";
+			document.getElementById('filter-value').value = "";
+			// Increment the select counter		
+			filterCount++;
+		} else {
+		showToast(`Attention`, `Please ensure all fields contain entries to add to the filter query`);
+		}
+
 });
 
 // Click event for filter date button
@@ -134,9 +171,9 @@ document.getElementById('add-top-item').addEventListener('click', () => {
 // Click event for generate query button
 document.getElementById('generate-query').addEventListener('click', () => {
 	console.log('Generate Query clicked');
-	const queryContainer = document.getElementById('query');
-	(queryContainer.childElementCount > 0) ? clearList(queryContainer) : false;
-	const queryHtml = buildQueryString(selects, filters);
+	const queryContainer = document.getElementById('query'),
+		queryHtml = buildQueryString(selects, filters);
+	(queryContainer.innerText.length > 0) ? queryContainer.innerText = '' : false;
 	queryContainer.insertAdjacentHTML('afterbegin', queryHtml);
 });
 

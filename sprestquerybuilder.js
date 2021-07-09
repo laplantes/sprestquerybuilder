881cd05344757644422da9f1ad6834ae6ff83c0f
Scott LaPlante <18195024+laplantes@users.noreply.github.com>
2021-07-09 16:02:17 -0400

    sprestquerybuilder.js - Refactored code for insertText function to leave the inserted text unselected and the cursor at the end of the insertion. Added click events for all filter actions buttons, and the aad to filter filter button. Removed old filter click event and function as it will no longer be used.

--- sprestquerybuilder.js
+++ sprestquerybuilder.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const filterStringContainer = document.getElementById('filter-string');
+
 let selects = {},
 	selectCount = 0,
 	filters = {},
@@ -142,11 +144,21 @@ const buildQueryString = (querystringSelects=``, querystringFilters=``, orderby=
 	return fullQueryString;
 };
 
-// add event listeners for buttons
+
 // ################################
 // Clear the info bar message body
 //  TODO: add this to the docs for the info bar
 // document.getElementById('close-notice').addEventListener('click', clearInfoBar);
+
+const testUrl = (queryString) => {
+	const testListName = document.getElementById('test-list-name').value,
+	testUrl = document.getElementById('test-url').value;
+	if (testListName && testUrl) {
+		window.open(`${testUrl}/_api/web/lists/getByTitle('${testListName}')/items/${queryString}`);
+	} else {
+		showToast('Attention', 'Ensure all fields contain entries before attempting to test the query');
+	}
+};
 
 
 const clearQueryContainers = (containers) => {
@@ -157,7 +169,6 @@ const clearQueryContainers = (containers) => {
 
 // Click event for select button
 document.getElementById('add-select-item').addEventListener('click', () => {
-
 	// get the select input value and convert any spaces to use the SharePoint format for spaces
 	const selectValue = document.getElementById('select-column-name').value.replace(/ /g, '_x0020_');
 	// Ensure there is content to add
@@ -181,12 +192,6 @@ document.getElementById('add-select-item').addEventListener('click', () => {
 	} else {
 		showToast(`Attention`, `Enter a column name to add to the select query`);
 	}
-});
-
-
-// Click event for filter date button
-document.getElementById('add-date-filter-item').addEventListener('click', () => {
-	console.log('Add filter date item clicked');
 });
 
 // Click event for orderby button TODO: add comments
@@ -239,27 +244,68 @@ document.getElementById('generate-query').addEventListener('click', () => {
 	queryContainer.innerText = queryCompleteString;
 });
 
-const testUrl = (queryString) => {
-	const testListName = document.getElementById('test-list-name').value,
-	testUrl = document.getElementById('test-url').value;
-	if (testListName && testUrl) {
-		console.log('Both exist');
-		window.open(`${testUrl}/_api/web/lists/getByTitle('${testListName}')/items/${queryString}`);
-	} else {
-		showToast('Attention', 'Ensure all fields contain entries before attempting to test the query');
-	}
-};
-
 // insert text into the specified location, or currently active location
-const insertText = (text, insertLocation = document.activeElement) => {
-	insertLocation.setRangeText(text, insertLocation.selectionStart, insertLocation.selectionEnd, 'select');
+const insertText = (text, insertLocation) => {
+	insertLocation.focus();
+	const start = insertLocation.selectionStart,
+		end = insertLocation.selectionEnd;
+	insertLocation.setRangeText(text, start, end)
+	// use the three lines below to insert text and leave cursor at end of inserted area
+	insertLocation.focus();
+	insertLocation.selectionStart = end + text.length;
+	insertLocation.selectionEnd = end + text.length;
 };
 
-// click event for clear query button
+//  Click event for 'open selection'
+document.getElementById('button-filter-open').addEventListener('click', () => {
+	insertText('(', filterStringContainer);
+});
+
+//  Click event for filter add 'and'
+document.getElementById('button-filter-and').addEventListener('click', () => {
+	insertText('and', filterStringContainer);
+});
+
+//  Click event for add 'or'
+document.getElementById('button-filter-or').addEventListener('click', () => {
+	insertText('or', filterStringContainer);
+});
+
+//  Click event for 'close selection'
+document.getElementById('button-filter-close').addEventListener('click', () => {
+	insertText(')', filterStringContainer);
+});
+
+// Click event for add to filter button
+document.getElementById('add-filter-query-item').addEventListener('click', () => {
+	const columnName = document.getElementById('filter-column-name').value,
+		operator = document.getElementById('filter-operator').value,
+		value = document.getElementById('filter-value').value,
+		date = document.getElementById('filter-date-value').value,
+		time = document.getElementById('filter-time-value').value;
+		console.log(`Go values of: ${columnName}, ${operator}, ${value}, ${date}, ${time}`);
+});
+
+//  Click event for filter clear all button
+document.getElementById('filter-clear-all').addEventListener('click', () => {
+	const clearConfirm = confirm('Are you sure you want to clear the contents of filter and start completely over?');
+	if (clearConfirm) {
+		document.getElementById('filter-string').value = ``;
+	} else {
+		showToast('Info', 'Canceled');
+	}
+});
+// click event for Remove all query items button
 document.getElementById('query-clear').addEventListener('click', () => {
-	let containersToClear = Array.from(document.getElementById('query-box-preview').children);
-	clearQueryContainers(containersToClear);
-	document.getElementById('query').innerText = '';
+	const clearConfirm = confirm('Are you sure you want to remove all the query items and start over?');
+	if (clearConfirm) {
+		let containersToClear = Array.from(document.getElementById('query-box-preview').children);
+		clearQueryContainers(containersToClear);
+		document.getElementById('query').innerText = '';
+		clearValues();
+	} else {
+		showToast('Info', 'Canceled');
+	}
 });
 
 //  click event for copy to clipboard
@@ -274,6 +320,7 @@ document.getElementById('copy-query').addEventListener('click', () => {
 			showToast(`Oh no`, `Query failed to copy to the clipboard`);
 		}
 	});
+});
 
 // Click event for test url button
 document.getElementById('test-url-button').addEventListener('click', () => {
@@ -284,40 +331,4 @@ document.getElementById('test-url-button').addEventListener('click', () => {
 	} else {
 		showToast('Attention', 'Generate a query before attempting to test it');
 	}
-});
-
-
-
-
-	// Click event for filter button
-	// document.getElementById('add-filter-item').addEventListener('click', () => {
-	// 	console.log('Add filter item clicked');
-	// 	// get the filter values
-	// 	const filterName = document.getElementById('filter-column-name').value.replace(/ /g, '_x0020_'),
-	// 		filterOperator = document.getElementById('filter-operator').value,
-	// 		filterValue = document.getElementById('filter-value').value,
-	// 		filterAddOr = document.getElementById('filter-item-and-or').value;
-	// 		if (filterName && filterOperator && filterValue) {
-	// 			// build the new object using the global filterCount and the functionally scoped filter values
-	// 			const newFilter = {id: filterCount, name: filterName, operator: filterOperator, value: `'${filterValue}'`, option: filterAddOr};
-	// 			// Call the function to build out the required HTML to add to the filter to the page
-	// 			const newFilterHtml = buildItemHtml(`filter`, newFilter);
-	// 			// Insert the newly created HTML
-	// 			document.getElementById('query-box-preview').insertAdjacentHTML('beforeend', newFilterHtml);
-	// 			// add the new filter into the filters object
-	// 			filters[`${filterCount}`] = newFilter;
-	// 			// Get the newly created HTML element
-	// 			const newFilterItem = document.getElementById(`filter-${filterCount}`);
-	// 			// Add an event listener to the newly created items remove button for removing the item from the page and from the selects object
-	// 			document.getElementById(`close-filter-${filterCount}`).addEventListener('click', () => { newFilterItem.remove(); delete filters[`${newFilter.id}`]; });
-	// 			// clear the inputs in the form in preparation for the next item to be added
-	// 			document.getElementById('filter-column-name').value = "";
-	// 			document.getElementById('filter-operator').value = "";
-	// 			document.getElementById('filter-value').value = "";
-	// 			// Increment the select counter		
-	// 			filterCount++;
-	// 		} else {
-	// 		showToast(`Attention`, `Ensure all fields contain entries to add to the filter query`);
-	// 		}
-	// });
 });
